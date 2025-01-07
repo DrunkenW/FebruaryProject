d33d3048148807ecc2043e1124bccea56646c062
Drunken <jugger2205@gmail.com>
2025-01-07 19:42:03 +0500

    координально изменил систему карты( добавил тип стен 1, 2)

--- map.py
+++ map.py
@@ -1,40 +1,23 @@
 from settings import *
 
+text_map = [
+    '111111111111',
+    '1......1...1',
+    '1..111...1.1',
+    '1....1..11.1',
+    '1..2....1..1',
+    '1..2...111.1',
+    '1....1.....1',
+    '1111111111111'
+]
 
-class Map:
-    def __init__(self):
-        self.text_map = [
-            'WWWWWWWWWWWW',
-            'W......W...W',
-            'W..WWW...W.W',
-            'W....W..WW.W',
-            'W..W....W..W',
-            'W..W...WWW.W',
-            'W....W.....W',
-            'WWWWWWWWWWWW'
-        ]
-
-        self.world_map = set()
-        self.mini_map = set()
-        self.WALL_SIZE = WALL_SIZE
-        self.create_map() # заполнение self.world_map объектами
-
-    def create_map(self):
-        for j, row in enumerate(self.text_map):
-            for i, obj in enumerate(row):
-                if obj == "W":
-                    self.world_map.add((i * self.WALL_SIZE, j * self.WALL_SIZE))
-                    self.mini_map.add((i * 20, j * 20))
-
-    def draw_map(self, sc):
-        # отрисовка стен
-        for wall in self.world_map:
-            x, y = wall
-            wall_rect = pygame.Rect(x, y, self.WALL_SIZE, self.WALL_SIZE)
-            pygame.draw.rect(sc, DARKGRAY, wall_rect, 1)
-
-    def get_map(self):
-        return self.world_map
-
-    def get_minimap(self):
-        return self.mini_map
+world_map = {}
+mini_map = set()
+for j, row in enumerate(text_map):
+    for i, char in enumerate(row):
+        if char != '.':
+            mini_map.add((i * WALL_SIZE / 5, j * WALL_SIZE / 5))
+            if char == '1':
+                world_map[(i * WALL_SIZE, j * WALL_SIZE)] = '1'
+            elif char == '2':
+                world_map[(i * WALL_SIZE, j * WALL_SIZE)] = '2'
